ff5247a3440d6e1a4c7e381f84a99725af3ec670
maride <git@maride.cc>
2020-05-04 15:10:53 +0200

    Fix warnings in 'open'

--- src/open.c
+++ src/open.c
@@ -4,7 +4,7 @@
 #include <dlfcn.h>
 
 int open(const char *pathname, int flags) {
-	FILE *(*original_fopen)(const char*, const char*);
+	int (*original_fopen)(const char*, int);
 	original_fopen = dlsym(RTLD_NEXT, "open");
 	int fh = (*original_fopen)(pathname, flags);
 	printf("Opening file %s to file handle %i\n", pathname, fh);
--- src/open.h
+++ src/open.h
@@ -1,6 +1,8 @@
 #ifndef __open_h__
 #define __open_h__
 
+#include <stdio.h>
+
 int open(const char *pathname, int flags);
 
 #endif // __open_h__
